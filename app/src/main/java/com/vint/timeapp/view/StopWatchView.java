3faa095f905abd0ac892f6ba43e38f9ae487f5cf
gutsul27 <GtigortsevichYuriy@gmail.com>
2017-10-29 18:18:47 +0200

    Added stopwatch layout.

---
+++ app/src/main/java/com/vint/timeapp/view/StopWatchView.java
@@ -0,0 +1,15 @@
+package com.vint.timeapp.view;
+
+/**
+ * Created by ygrigortsevich on 08.03.17.
+ */
+
+public interface StopWatchView extends BaseView {
+
+    void start();
+
+    void pause();
+
+    void reset();
+
+}
