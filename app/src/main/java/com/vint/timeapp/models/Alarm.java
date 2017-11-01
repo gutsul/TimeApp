da20fbacf5072e365e2d853a073346d34470d1ec
gutsul27 <GtigortsevichYuriy@gmail.com>
2017-11-01 21:34:01 +0200

    Added alarm model.

---
+++ app/src/main/java/com/vint/timeapp/models/Alarm.java
@@ -0,0 +1,48 @@
+package com.vint.timeapp.models;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by gutsul on 01.11.17.
+ */
+
+public class Alarm extends RealmObject {
+    @PrimaryKey
+    private String id;
+    private long time;
+    private String message;
+    boolean isEnable = true;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isEnable() {
+        return isEnable;
+    }
+
+    public void setEnable(boolean enable) {
+        isEnable = enable;
+    }
+}
