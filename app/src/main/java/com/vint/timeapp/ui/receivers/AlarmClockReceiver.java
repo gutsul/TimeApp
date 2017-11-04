0be53f41ecd67be5ebe2bfd08db71cff9584adb0
gutsul27 <GtigortsevichYuriy@gmail.com>
2017-11-04 18:24:24 +0200

    Refactor.

--- app/src/main/java/com/vint/timeapp/ui/receivers/AlarmClockReceiver.java
+++ app/src/main/java/com/vint/timeapp/ui/receivers/AlarmClockReceiver.java
@@ -18,7 +18,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
